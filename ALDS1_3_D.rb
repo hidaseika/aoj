73965538ca4af3939110c3cc6e8f60edcdb6dfb9
Seika Hida <hidaseika@realglobe.jp>
2018-12-26 18:29:06 +0900

    途中

--- ALDS1_3_D.rb
+++ ALDS1_3_D.rb
@@ -1,4 +1,18 @@
 ground = gets.split("")
+h = 0
+ground = ground.map do |g|
+  if g == "/"
+    h += 1
+  elsif g == "_"
+    h
+  else
+    h -= 1
+  end
+end
+
+print ground
+# \\///\_/\/\\\\/_/\\///__\\\_\\/_\/_/\
+# [-1, -2, -1, 0, 1, 0, 0, 1, 0, 1, 0, -1, -2, -3, -2, -2, -1, -2, -3, -2, -1, 0, 0, 0, -1, -2, -3, -3, -4, -5, -4, -4, -5, -4, -4, -3, -4, -5]
 
 class Puddle
   attr_accessor :size
@@ -20,4 +34,3 @@ class Puddle
     end
   end
 end
-# まだ途中
