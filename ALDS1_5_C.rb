9a3d0c6289cf42bb5b49a1cadd6d19d03f44d204
Seika Hida <hidaseika@realglobe.jp>
2019-03-06 17:53:59 +0900

    できた

--- ALDS1_5_C.rb
+++ ALDS1_5_C.rb
@@ -1,6 +1,5 @@
 N = gets.to_i
 
-
 class Vertex
   attr_accessor :x, :y
   def initialize(x, y)
@@ -46,13 +45,19 @@ class Edge
     @end_vertex = end_vertex
   end
 
-  def two_vertex_to_five(edge)
-    v1 = edge.start_vertex
-    v5 = edge.end_vertex
+  def two_vertex_to_five
+    v1 = @start_vertex
+    v5 = @end_vertex
     v2 = (v1 * 2 + v5) / 3
     v4 = (v1 + v5 * 2) / 3
     v3 = v2 + (v4 - v2).rotate(Math::PI / 3)
     [v1,v2,v3,v4,v5]
+  end
+
+  # [Edge, Edge, Edge, Edge, Edge]
+  def divide
+    vertexes = two_vertex_to_five
+    vertexes[0..-2].zip(vertexes[1..-1]).map {|v1, v2| Edge.new(v1, v2)}
   end
 end
 
@@ -60,22 +65,23 @@ end
 class KochCurve
   def initialize(n)
     @n = n
-  end
-
-  def create_koch_curve
-    # 続きはここから考える
-    first_edge = Edge.new(Vertex.new(0, 0), Vertex.new(100, 0))
-    koch_curves = [first_edge]
-    
-  end
-
-  def vertexes # [Vertex, Vertex, ]
+    @edges = [Edge.new(Vertex.new(0, 0), Vertex.new(100, 0))]
     create_koch_curve
   end
 
-  
+  def create_koch_curve
+    @n.times do
+      @edges = @edges.map(&:divide).flatten
+    end
+    @edges
+  end
+
+  def vertexes # [Vertex, Vertex, ]
+    @edges.map {|edge| edge.start_vertex} + [Vertex.new(100, 0)]
+  end
 end
 
-koch_curves.vertexes.each do |vertex|
+koch_curve = KochCurve.new(N)
+koch_curve.vertexes.each do |vertex|
   puts "#{vertex.x} #{vertex.y}"
 end
